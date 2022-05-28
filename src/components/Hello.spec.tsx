112bb6d3a0b0a707d86968a9a8be54f5332dcc9e
ChrisCoastal <contactchriscoastal@gmail.com>
2022-05-28 11:19:36 -0700

    add lint-staged

--- src/components/Hello.spec.tsx
+++ src/components/Hello.spec.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Hello } from './Hello';
+import React from "react";
+import { Hello } from "./Hello";
 
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 
 it('renders string "Hello!"', () => {
   render(<Hello />);
-  const myElement = screen.getByText('Hello!');
+
+  const myElement = screen.getByText("Hello!");
   expect(myElement).toBeInTheDocument(); // requires setup-jest.ts   import '@testing-library/jest-dom';
 });
